89a2d458c7d81f01beb94a7ca2164be395bafde3
Neha Kennard <nnayak@umass.edu>
2023-04-29 22:53:13 +0000

    inorder walk seems to work but needs cleaning

--- convert_data.py
+++ convert_data.py
@@ -35,14 +35,56 @@ def print_edus(tree, indent=0):
     #print("~")
 
 
+def convert_to_segmentation_format(parsed):
+  pass
+
+
+def get_edu_list(parse):
+
+  edus = [None]
+
+  def inorder_walk(tree):
+    current_edu = []
+    for i, x in enumerate(tree):
+      if type(x) in [str, int, float]:
+        if type(x) == str and tree[0] == 'text' and i > 0:
+          p = x.replace("_!", "")
+          if x.startswith("_!"):
+            current_edu.append(p)
+          else:
+            current_edu.append(p)
+        if type(x) == str and x.endswith("_!"):
+          edus.append(current_edu)
+          current_edu = []
+      else:
+        inorder_walk(x)
+
+  inorder_walk(parse)
+
+  for i, y in enumerate(edus):
+    print(i, y)
+    
+    
+  
+
+
+class GoldRSTParse(object):
+  def __init__(self, parse_text):
+    parse = lisp_lib.parse(parse_text)
+    edu_list = get_edu_list(parse)
+    pass
+
+
 def main():
 
   for filename in glob.glob(TRAINING_PATH):
     with open(filename, 'r') as f:
       text = f.read()
-      parsed = lisp_lib.parse(text)
-      print_edus(parsed)
-      print()
+      _ = GoldRSTParse(text)
+      #parsed = lisp_lib.parse(text)
+      #convert_to_segmentation_format(parsed)
+      #print_edus(parsed)
+      #print()
 
 
 
